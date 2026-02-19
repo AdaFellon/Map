d5967ae22419f1bb263ad33a7c9ad88ba1b76daa
AdaFellon <gataullina_lara@mail.ru>
2026-02-19 19:19:25 +0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,286 @@
+const locations = [
+
+/* ================= 1941 ================= */
+
+{
+  name: "Дулаг-130",
+  place: "Рославль, Смоленская область",
+  year: "1941",
+  type: "Дулаг",
+  lat: 53.952,
+  lng: 32.863,
+  image: "https://upload.wikimedia.org/wikipedia/commons/3/3d/Roslavl.jpg",
+  description: "Немецкий пересыльный лагерь для военнопленных, действовал в 1941–1943 гг."
+},
+
+{
+  name: "Бабий Яр",
+  place: "Киев, Украина",
+  year: "1941",
+  type: "Массовое убийство",
+  lat: 50.471,
+  lng: 30.448,
+  image: "https://upload.wikimedia.org/wikipedia/commons/3/3c/Babi_Yar_memorial.jpg",
+  description: "29–30 сентября 1941 года нацистами было расстреляно более 33 000 человек."
+},
+
+{
+  name: "Яновский лагерь",
+  place: "Львов, Украина",
+  year: "1941",
+  type: "Концлагерь",
+  lat: 49.8397,
+  lng: 24.0297,
+  image: "https://upload.wikimedia.org/wikipedia/commons/2/2f/Lviv_Janowska.jpg",
+  description: "Концлагерь на окраине Львова, действовал в 1941–1944 гг."
+},
+
+{
+  name: "Шталаг-352",
+  place: "Масюковщина (Минск, Беларусь)",
+  year: "1941",
+  type: "Концлагерь",
+  lat: 53.933,
+  lng: 27.460,
+  image: "https://upload.wikimedia.org/wikipedia/commons/5/5c/Stalag352_memorial.jpg",
+  description: "Крупный лагерь для советских военнопленных."
+},
+
+{
+  name: "Малый Тростенец",
+  place: "Минск, Беларусь",
+  year: "1941",
+  type: "Концлагерь",
+  lat: 53.840,
+  lng: 27.690,
+  image: "https://upload.wikimedia.org/wikipedia/commons/4/4e/Maly_Trostinets.jpg",
+  description: "Один из крупнейших лагерей смерти на территории Беларуси."
+},
+
+{
+  name: "Концлагерь Богдановка",
+  place: "Николаевская область, Украина",
+  year: "1941",
+  type: "Концлагерь",
+  lat: 47.833,
+  lng: 31.300,
+  image: "https://upload.wikimedia.org/wikipedia/commons/6/6e/Bogdanovka_memorial.jpg",
+  description: "Место массовых расстрелов у села Богдановка."
+},
+
+{
+  name: "Сырецкий концлагерь",
+  place: "Киев (Сырец), Украина",
+  year: "1941",
+  type: "Концлагерь",
+  lat: 50.476,
+  lng: 30.431,
+  image: "https://upload.wikimedia.org/wikipedia/commons/1/1a/Syrets_camp_memorial.jpg",
+  description: "Концентрационный лагерь рядом с Бабьим Яром."
+},
+
+{
+  name: "Саласпилс",
+  place: "Саласпилс, Латвия",
+  year: "1941",
+  type: "Концлагерь",
+  lat: 56.861,
+  lng: 24.349,
+  image: "https://upload.wikimedia.org/wikipedia/commons/8/8a/Salaspils_memorial.jpg",
+  description: "Концлагерь и место массовых убийств 1941–1944 гг."
+},
+
+{
+  name: "Колдычево",
+  place: "Брестская область, Беларусь",
+  year: "1941",
+  type: "Концлагерь",
+  lat: 53.120,
+  lng: 25.970,
+  image: "https://upload.wikimedia.org/wikipedia/commons/2/28/Koldychevo_memorial.jpg",
+  description: "Концентрационный лагерь в Барановичском районе."
+},
+
+/* ================= 1942 ================= */
+
+{
+  name: "Дулаг-142",
+  place: "Брянск, Россия",
+  year: "1942",
+  type: "Дулаг",
+  lat: 53.252,
+  lng: 34.371,
+  image: "https://upload.wikimedia.org/wikipedia/commons/5/57/Bryansk.jpg",
+  description: "Пересыльный лагерь для военнопленных."
+},
+
+{
+  name: "Дулаг-205",
+  place: "Алексеевка, Россия",
+  year: "1942",
+  type: "Дулаг",
+  lat: 50.630,
+  lng: 38.690,
+  image: "https://upload.wikimedia.org/wikipedia/commons/0/08/Alekseevka.jpg",
+  description: "Немецкий лагерь для военнопленных."
+},
+
+{
+  name: "Шталаг 1-Д",
+  place: "Пригородное (Калининградская область)",
+  year: "1942",
+  type: "Концлагерь",
+  lat: 54.700,
+  lng: 20.500,
+  image: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Kaliningrad.jpg",
+  description: "Лагерь для военнопленных."
+},
+
+{
+  name: "Змиёвская балка",
+  place: "Ростов-на-Дону, Россия",
+  year: "1942",
+  type: "Массовое убийство",
+  lat: 47.258,
+  lng: 39.650,
+  image: "https://upload.wikimedia.org/wikipedia/commons/6/6a/Zmievskaya_Balka_memorial.jpg",
+  description: "Место массового расстрела мирного населения."
+},
+
+{
+  name: "Жестяная Горка",
+  place: "Новгородская область, Россия",
+  year: "1942",
+  type: "Массовое убийство",
+  lat: 58.6333,
+  lng: 30.3833,
+  image: "https://upload.wikimedia.org/wikipedia/commons/6/6f/Zhestyana_Gorka_memorial.jpg",
+  description: "Место массовых убийств 1942–1944 гг."
+},
+
+{
+  name: "Концлагерь «Красный»",
+  place: "Мирное, Крым",
+  year: "1942",
+  type: "Концлагерь",
+  lat: 44.9675,
+  lng: 34.0483,
+  image: "https://upload.wikimedia.org/wikipedia/commons/0/05/Krasny_concentration_camp.jpg",
+  description: "Крупнейший лагерь смерти в Крыму."
+},
+
+{
+  name: "Детский концлагерь",
+  place: "Вырица, Ленинградская область",
+  year: "1942",
+  type: "Детский лагерь",
+  lat: 59.407,
+  lng: 30.344,
+  image: "https://upload.wikimedia.org/wikipedia/commons/4/4d/Vyritsa.jpg",
+  description: "Лагерь для детей в посёлке Вырица."
+},
+
+{
+  name: "Майданек",
+  place: "Люблин, Польша",
+  year: "1942",
+  type: "Концлагерь",
+  lat: 51.246,
+  lng: 22.568,
+  image: "https://upload.wikimedia.org/wikipedia/commons/5/5a/Majdanek.jpg",
+  description: "Концлагерь на окраине Люблина."
+},
+
+/* ================= 1943 ================= */
+
+{
+  name: "Хатынь",
+  place: "Минская область, Беларусь",
+  year: "1943",
+  type: "Массовое убийство",
+  lat: 54.334,
+  lng: 27.866,
+  image: "https://upload.wikimedia.org/wikipedia/commons/8/8b/Khatyn_memorial.jpg",
+  description: "22 марта 1943 года деревня была сожжена. Погибли 149 человек."
+},
+
+{
+  name: "Корюковская трагедия",
+  place: "Корюковка, Украина",
+  year: "1943",
+  type: "Массовое убийство",
+  lat: 51.7500,
+  lng: 31.8333,
+  image: "https://upload.wikimedia.org/wikipedia/commons/3/33/Koryukovka_memorial.jpg",
+  description: "1–2 марта 1943 года уничтожено более 6 700 жителей."
+},
+
+{
+  name: "Саласпилс",
+  place: "Саласпилс, Латвия",
+  year: "1943",
+  type: "Концлагерь",
+  lat: 56.861,
+  lng: 24.349,
+  image: "https://upload.wikimedia.org/wikipedia/commons/8/8a/Salaspils_memorial.jpg",
+  description: "Концлагерь и место массовых убийств."
+},
+
+{
+  name: "Концлагерь «Красный»",
+  place: "Мирное, Крым",
+  year: "1943",
+  type: "Концлагерь",
+  lat: 44.9675,
+  lng: 34.0483,
+  image: "https://upload.wikimedia.org/wikipedia/commons/0/05/Krasny_concentration_camp.jpg",
+  description: "Крупнейший лагерь смерти в Крыму."
+},
+
+{
+  name: "Жестяная Горка",
+  place: "Новгородская область, Россия",
+  year: "1943",
+  type: "Массовое убийство",
+  lat: 58.6333,
+  lng: 30.3833,
+  image: "https://upload.wikimedia.org/wikipedia/commons/6/6f/Zhestyana_Gorka_memorial.jpg",
+  description: "Массовые расстрелы мирного населения."
+},
+
+/* ================= 1944 ================= */
+
+{
+  name: "Майданек",
+  place: "Люблин, Польша",
+  year: "1944",
+  type: "Концлагерь",
+  lat: 51.246,
+  lng: 22.568,
+  image: "https://upload.wikimedia.org/wikipedia/commons/5/5a/Majdanek.jpg",
+  description: "Освобождён в 1944 году."
+},
+
+{
+  name: "Жестяная Горка",
+  place: "Новгородская область, Россия",
+  year: "1944",
+  type: "Массовое убийство",
+  lat: 58.6333,
+  lng: 30.3833,
+  image: "https://upload.wikimedia.org/wikipedia/commons/6/6f/Zhestyana_Gorka_memorial.jpg",
+  description: "Продолжение расследования массовых захоронений."
+},
+
+{
+  name: "Концлагерь «Красный»",
+  place: "Мирное, Крым",
+  year: "1944",
+  type: "Концлагерь",
+  lat: 44.9675,
+  lng: 34.0483,
+  image: "https://upload.wikimedia.org/wikipedia/commons/0/05/Krasny_concentration_camp.jpg",
+  description: "Освобождён в 1944 году."
+}
+
+];
