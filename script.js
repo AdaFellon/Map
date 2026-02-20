46d6a2e9a41394832864602edd92219745a3ad4d
AdaFellon <gataullina_lara@mail.ru>
2026-02-20 10:09:00 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const map = L.map('map').setView([53.5, 27.5], 5);
-// Граница СССР (точная подгонка под сайт)
 const ussrBounds = [
-    [33.5, 14],   // юго-запад
-    [83.5, 192]   // северо-восток
+    [25.0, -30.0],  // юго-запад
+    [90.0, 210.0]   // северо-восток
 ];
 
 L.imageOverlay("images/ussr_border.svg", ussrBounds, {
-    opacity: 0.72,
+    opacity: 0.6,
     interactive: false
 }).addTo(map);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
